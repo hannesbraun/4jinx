5a04f7839eb1b0665840c91bbe2b5f6871e074f7
Hannes Braun <hannesbraun@mail.de>
2020-07-21 14:01:45 +0200

    Fix formatting

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 )
 
 const (
-	BlueColor = "\033[1;34m%s\033[0;0m"
-	BlueColorRaw = "\033[1;34m"
-	RedColor = "\033[1;31m%s\033[0;0m"
-	RedColorRaw = "\033[1;31m"
+	BlueColor     = "\033[1;34m%s\033[0;0m"
+	BlueColorRaw  = "\033[1;34m"
+	RedColor      = "\033[1;31m%s\033[0;0m"
+	RedColorRaw   = "\033[1;31m"
 	ResetColorRaw = "\033[0;0m"
-	YellowColor = "\033[0;33m%s\033[0;0m"
+	YellowColor   = "\033[0;33m%s\033[0;0m"
 )
 
 func main() {
